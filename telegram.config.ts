9c41c3fc6aed66d15a4b0a4e7a94cf8d6c789ba6
House Kityky <orbit4ever@ukr.net>
2021-12-29 21:19:28 +0200

    revert commit

--- telegram.config.ts
+++ telegram.config.ts
@@ -1,5 +1,4 @@
 export default {
   TelegramBotToken:  process.env.TELEGRAM_BOT_API ?? '',
   ChatId: process.env.CHAT_ID ?? '',
-  // https://dev.to/francis04j/how-to-add-env-and-use-process-env-to-your-typescript-project-3d6b
 }
