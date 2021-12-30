a0de135881ec2147add0f91abb6fab039a97fa6f
House Kityky <orbit4ever@ukr.net>
2021-12-30 08:08:28 +0200

    disable reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ const config: PlaywrightTestConfig = {
             }
         }
     ],
-    reporter: [[process.env.CI ? 'github' : 'list'], ['./_support/telegram-reporter.ts']],
+    reporter: process.env.CI ? 'github' : 'list',
     testDir: './tests',
 }
 export default config;
